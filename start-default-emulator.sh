73e2965e46372c3a2922f1b0c3f2a2f4f4c3af3a
Edgar Kenji Yamamoto <edgar.yamamoto@gmail.com>
2021-02-10 15:12:01 -0300

    Fix path

--- start-default-emulator.sh
+++ start-default-emulator.sh
@@ -7,7 +7,7 @@ EMU_BOOTED="no"
 while [[ ${EMU_BOOTED} != *"stopped"* ]]; do
     echo "Wating for emulator booted..."
     sleep 5
-    EMU_BOOTED=$(adb shell getprop init.svc.bootanim || echo "no")
+    EMU_BOOTED=$(${ANDROID_SDK_ROOT}/cmdline-tools/tools/bin/adb shell getprop init.svc.bootanim || echo "no")
 done
 
 echo "Emulator booted"
